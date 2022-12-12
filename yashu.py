b48e029c9eac5f536add0ba595a939a332ea13ba
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-12-13 01:18:06 +0530

    Update yashu.py

--- yashu.py
+++ yashu.py
@@ -2,7 +2,7 @@ ID = 10763476
 HASH = "e7d6d5493a896264a09d04fda7a30f9d"
 
 STRING = "BQCOnKU0Tectcb1OEYC5PY-mA58fTgg1cHwWBx6H2igmRbrE_Hr0qBS-3weqmQ0Xrs-2Qj9vahvK2JXRNGAMQYesJc8AD3_Bkh5Xd2f2ZQXSMOdCZmBNe8D1T8RK33rRR-q7uxJu7GGFDO1jz88-7rbRdpfdRrULxOscJTaN9kftxAlQwsZSqeVMWy8V9u1xYwE9wVoTdC3hTETJTgDbUleclT5cVeHZ_PCIZRzpMKLWpT949_Jwno1C-QoIKPVqgq4n4bu85DJdx4a0gg6VQOTxik9QoXgUk6ATUFEW6i1X5YkE_4MB3rD-IDSctOcXob7akB8EGzv1h2lVs-u8_1lUSYfehQA"
-#
+
 MONGO = "mongodb+srv://musicbot:<password>@cluster0.61lydz4.mongodb.net/?retryWrites=true&w=majority"
 
 SUDO = "5868832590"
